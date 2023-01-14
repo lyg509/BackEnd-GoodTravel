15bae163142dee3eb8ad2e08ff14f3edbf75655c
lyg509 <dydrjs252@naver.com>
2023-01-14 14:31:04 +0900

    fix : 사용사 코스 관광지 수정

--- src/main/java/com/lyg/goodtravel/domain/course/db/entity/CourseDataID.java
+++ src/main/java/com/lyg/goodtravel/domain/course/db/entity/CourseDataID.java
@@ -4,12 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CourseDataID implements Serializable {
+
+    @Column(name = "course_id")
     int courseId;
+
+    @Column(name = "course_data_id")
     int courseDataId;
 }
